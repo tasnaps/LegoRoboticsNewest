677eb4fa97e303724e6c883968dddfce7ce95b4d
Tapio Meriläinen <tapiom4@gmail.com>
2022-11-28 17:09:04 +0200

    Changed lego robot program code drastically The robot now calculates location divided on two sides of the arena. we know which way to move and look for black line to detect in third IR sensor. Need to measure ir sensor in practice to know which values to use when stopping the robot.
    When we meet the line, we turn towards the goal and move forward.

--- main.py
+++ main.py
@@ -18,22 +18,29 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
-
 # DriveBase constructor values might need adjustments
 base = DriveBase(left_motor, right_motor, 60, 150)
+
+
 left_ir_sensor = Ev3devSensor(Port.S3)
 right_ir_sensor = Ev3devSensor(Port.S4)
-#compass sensor not in use
-compass_sensor = Ev3devSensor(Port.S1)
-#color_sensor = Ev3devSensor(Port.S1)
+
+#Center IR sensor for detecting the black line used in goal tracking
+center_ir_sensor = Ev3devSensor(Port.S2)
 
 left_ir_sensor_value = left_ir_sensor.read("DC")
 right_ir_sensor_value = right_ir_sensor.read("DC")
-lightSensor = LightSensor(Port.S2)
+center_ir_sensor_value = center_ir_sensor.read("DC")
 
-#compass_value = compass_sensor.read("GYRO")
-#light_sensor_value = light_sensor.read("REFLECT")#Available modes reflect and ambient
+#Light sensor optional for goal tracking instead of black line and ir sensor
+#lightSensor = LightSensor(Port.S2)
 
+# when we set the robot in arena, must face forward we update this in degrees turned
+heading = 0
+locationToLine = 0
+
+
+#Optional function for approaching goal
 def getGoalDir():
     print("Getting goal direction")
     lightValue = lightSensor.ambient()
@@ -49,6 +56,57 @@ def getGoalDir():
         base.turn(40)
 
 
+def getRotation():
+    #Cleaning data
+    newHeading = heading/360
+    while(newHeading>1 or newHeading<-1):
+        if newHeading>1:
+            newHeading =-1
+        if newHeading<-1:
+            newHeading =+1
+    #cleaned heading returning a decimal doesnt matter if positive or negative for now
+    #newHeading = newHeading * 360
+    return abs(newHeading)
+
+
+def updateLocation(distance):
+    tempHeading = getRotation
+    #Update on the location based on which side we have moved. 
+    if tempHeading<0.5:
+        locationToLine += distance
+    if tempHeading>0.5:
+        locationToLine -= distance
+
+        
+def moveToLine():
+    tempHeading = getRotation() * 360
+
+    #we are on the left side of the line
+    rotation = -tempHeading + 90
+    if locationToLine<0:
+        base.turn(rotation)
+        readAndMove()
+        base.turn(-90)
+        base.straight(10000)
+    
+    #we are on the right side of the line
+    if locationToLine>0:
+        base.turn(rotation+180)
+        readAndMove()
+        base.turn(90)
+        base.straight(10000)
+
+    #read the third sensor value and move forward until we hit the black line
+    #once we hit the line we turn depending on approach and move forward.
+
+def readAndMove():
+    irValue = center_ir_sensor.read("DC")
+    #TODO Need to check what value we get on black line 
+    if irValue < 255:
+        base.straight(50)
+        readAndMove()
+
+    
 
 def detectColors():
     color_sensor_value = color_sensor.read("RGB")
@@ -62,6 +120,10 @@ def detectColors():
     else:
         return "none"
 
+
+
+
+
 # one option is to only use one sensor, if we install it on the middle. Then the values should be ok when ball is in the middle.
 def movement(left_ir_sensor, right_ir_sensor):
     left_ir_sensor_value = left_ir_sensor.read("DC")
@@ -71,19 +133,22 @@ def movement(left_ir_sensor, right_ir_sensor):
     if(right_ir_sensor_value[0] == 0 and left_ir_sensor_value[0] == 0):
         print("ball behind")
         base.turn(40)
+        heading =+40
 
     if right_ir_sensor_value[0]>5:#6
         print("ball slightly to the right")
         base.turn(20)
+        heading =+20
         movement(left_ir_sensor, right_ir_sensor)
 
     if left_ir_sensor_value[0] < 5:#4
         print("ball slightly to the left")
         base.turn(-25)
+        heading =-20
         movement(left_ir_sensor, right_ir_sensor)
     else:
-        #base.straight(150)
-        getGoalDir()
+        base.straight(150)
+        updateLocation(150)
         movement(left_ir_sensor, right_ir_sensor)
 
 movement(left_ir_sensor, right_ir_sensor)
